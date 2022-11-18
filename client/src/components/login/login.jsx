d4a06e12a0c5ace8baabf92d2f1ccb0048c156f3
czhu67 <caitlin.zhu2010@gmail.com>
2022-11-18 08:55:41 -0800

    login component created-not yet imported

---
+++ client/src/components/login/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {useState, useEffect} from "react";
+import axios from 'axios';
+
+export default function Login () {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
