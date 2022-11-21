23d5df3588fcba9dc03658b6e194e8a823caf693
Ricardo Vargas <richi_v@aol.com>
2022-11-21 11:12:28 -0600

    Grabbing updates

--- client/src/components/carousel/ImageCarousel.jsx
+++ client/src/components/carousel/ImageCarousel.jsx
@@ -3,12 +3,14 @@ import styled, { css } from 'styled-components'
 
 const CarouselContainer = styled.div` //The Outer most div
 border: 1px solid black;
-max-width: '100vh';
-max-height: '100vh';
+/* max-width: '100vh';
+max-height: '100vh'; */
+max-width: 100%;
+max-height: 100%;
 overflow: hidden;
 `
 const InnerContainer = styled.div` //Nested inside the container
-box-sizing: content-box;
+box-sizing: border-box;
 position: relative;
 display: flex;
 flex-direction: row;
@@ -27,13 +29,14 @@ width: ${props => props.width ? props.width : '225px'};
 height: ${props => props.height ? props.height : '199px'};
 `
 const Data = styled.img`
+object-fit: cover;
 width: 211px;
 height: 188px;
 `
 
 const ButtonR = styled.div`
 position: absolute;
-color: #d012c3;
+color: black;
 right 2%;
 top: 45%;
 cursor: pointer;
@@ -44,17 +47,28 @@ position: absolute;
 top: 45%;
 left: 2%;
 cursor: pointer;
-color: #d012c3;
+color: black;
 `
 
 //Set the Innercontainer to be the width of however many cards are needed to be displayed
 const ImageCarousel = ({ data }) => {
+  const [active, setActive] = useState(0);
+
+  const handleRight = () => {
+    const card = document.querySelector('#inner-container')
+    console.log(card)
+  }
+  const handleLeft = () => {
+    console.log('left')
+  }
+
+
 
   return (
     <CarouselContainer>
-      <InnerContainer>
-        <ButtonL>PREV</ButtonL>
-        <ButtonR>NEXT</ButtonR>
+      <InnerContainer id='inner-container'>
+        <ButtonL onClick={handleLeft}>PREV</ButtonL>
+        <ButtonR onClick={handleRight}>NEXT</ButtonR>
         {data.map((pic, i) =>
           <TreeCard key={i}>
             <Data src={pic} />
