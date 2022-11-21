4c1c6f756216bf4a19e535e59aaa24a54aaa0ae1
czhu67 <caitlin.zhu2010@gmail.com>
2022-11-21 14:45:14 -0800

    more styling, create account and exisitng user button functionality

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -7,6 +7,7 @@ import trees from './trees.png';
 export default function Login ({ toggleModal }) {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
+  const [warning, setWarning] = useState('');
 
   var login = (e) => {
     e.preventDefault();
@@ -14,15 +15,21 @@ export default function Login ({ toggleModal }) {
     if (emailCheck.test(inputEmail) && inputPassword !== '') {
       axios.post('/login', {email: inputEmail, pwd: inputPassword}).then((data) => {
         if (data.data.indexOf('does not exist') === -1 && data.data !== 'Incorrect password. Please try again.') {
-          console.log(data.data);
+          console.log('Success!');
+          toggleModal('login');
+        } else if (data.data === 'Incorrect password. Please try again.') {
+          setWarning('wrong password');
         } else {
-          console.log('ERROR: ', data.data);
+          setWarning('no account');
         }
+        document.querySelector('#password').value = '';
       })
     } else {
-      console.log('Invalid email/password.');
+      setWarning('invalid');
       document.querySelector('#password').value = '';
     }
+    setInputEmail('');
+    setInputPassword('');
   }
 
   return (
@@ -36,19 +43,26 @@ export default function Login ({ toggleModal }) {
           e.stopPropagation();
         }}
       >
-        <h2>Log In</h2>
+        <h1>TravelGreen</h1>
         <img src={trees}/>
         <div className="form">
           <div className="modalBody">
+            <h3>Log In</h3>
             <label>Email</label><br/>
             <input onChange={(e) => setInputEmail(e.target.value)}/><br/>
             <label>Password</label><br/>
             <input id="password" type="password" onChange={(e) => setInputPassword(e.target.value)}/><br/>
+            {warning === 'invalid' ? <div className="warning">Invalid e-mail/password.</div> : null}
+            {warning === 'no account' ? <div className="warning">Account not found.</div> : null}
+            {warning === 'wrong password' ? <div className="warning">Incorrect password. Please try again.</div> : null}
           </div>
           <div className="modalFooter">
             <button onClick={login}>Log In</button><br/>
             OR
-            <br/><button>Create Account</button>
+            <br/><button onClick={() => {
+              toggleModal('login');
+              toggleModal('signup');
+            }}>Create Account</button>
           </div>
         </div>
       </div>
